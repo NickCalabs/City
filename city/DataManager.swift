653ed1824cf6579b3c954d3b4eaa7399936072db
Nick <calabro.nick@gmail.com>
2015-04-01 23:50:37 -0400

    Initial commit

---
+++ city/DataManager.swift
@@ -0,0 +1,59 @@
+//
+//  DataManager.swift
+//  city
+//
+//  Created by Nick on 4/1/15.
+//  Copyright (c) 2015 Nick. All rights reserved.
+//
+
+import Foundation
+
+let CityImage = "http://www.reddit.com/r/CityPorn.json"
+
+class DataManager {
+    
+    
+    class func getTopImagesFromCityPornWithSuccess(success: ((iTunesData: NSData!) -> Void)) {
+        //1
+        loadDataFromURL(NSURL(string: CityImage)!, completion:{(data, error) -> Void in
+            //2
+            if let urlData = data {
+                //3
+                success(iTunesData: urlData)
+            }
+        })
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    class func loadDataFromURL(url: NSURL, completion:(data: NSData?, error: NSError?) -> Void) {
+        var session = NSURLSession.sharedSession()
+        
+        // Use NSURLSession to get data from an NSURL
+        let loadDataTask = session.dataTaskWithURL(url, completionHandler: { (data: NSData!, response: NSURLResponse!, error: NSError!) -> Void in
+            if let responseError = error {
+                completion(data: nil, error: responseError)
+            } else if let httpResponse = response as? NSHTTPURLResponse {
+                if httpResponse.statusCode != 200 {
+                    var statusError = NSError(domain:"com.raywenderlich", code:httpResponse.statusCode, userInfo:[NSLocalizedDescriptionKey : "HTTP status code has unexpected value."])
+                    completion(data: nil, error: statusError)
+                } else {
+                    completion(data: data, error: nil)
+                }
+            }
+        })
+        
+        loadDataTask.resume()
+    }
+    
+    
+    
+}
--- city/ViewController.swift
+++ city/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var cityImage: UIImageView!
+    @IBOutlet weak var cityChoice1: UIButton!
+    @IBOutlet weak var cityChoice2: UIButton!
+    @IBOutlet weak var cityChoice3: UIButton!
+    @IBOutlet weak var cityChoice4: UIButton!
+    
+    var URLarr:[String] = []
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor(red:0.24, green:0.28, blue:0.31, alpha:1)
+        UIApplication.sharedApplication().setStatusBarStyle(UIStatusBarStyle.LightContent, animated: true)
+        
+        
+        DataManager.getTopImagesFromCityPornWithSuccess { (redditData) -> Void in
+            let json = JSON(data:redditData)
+            
+            for (var i = 0; i < 500; i++){
+                if let cityPic = json["data"]["children"][i]["data"]["url"].stringValue as String? {
+                    if cityPic.rangeOfString("http://i.") != nil || cityPic.rangeOfString(".jpg") != nil {
+                        println("NSURLSession: \(cityPic)")
+                        self.URLarr.append(cityPic)
+                    }
+                }
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func test(sender: AnyObject) {
+        let url = NSURL(string: URLarr[1])
+        let data = NSData(contentsOfURL: url!)
+        cityImage.image = UIImage(data: data!)
+    }
+    
+    
+   
 }
 
